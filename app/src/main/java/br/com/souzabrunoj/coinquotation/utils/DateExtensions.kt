95ecc52d4b4e6111fa9c1dfb501f3afb779a25f7
Bruno Souza <souza.brunoj@gmail.com>
2023-07-06 16:16:32 -0300

    Fix treatment for null value in format date extension.

--- app/src/main/java/br/com/souzabrunoj/coinquotation/utils/DateExtensions.kt
+++ app/src/main/java/br/com/souzabrunoj/coinquotation/utils/DateExtensions.kt
@@ -7,8 +7,15 @@ private const val DATE_FORMAT = "yyyy-MM-dd"
 private const val DATE_FORMATTER = "dd-MM-yyyy"
 
 fun String.toLocaleDateFormat(): String {
-    val locale = Locale.getDefault()
-    val formatter = SimpleDateFormat(DATE_FORMATTER, locale)
-    val parse = SimpleDateFormat(DATE_FORMAT, locale).parse(this)
-    return if (isNotEmpty() && parse != null) formatter.format(parse) else " - "
+    val defaultValue = " - "
+    return if (isNullOrEmpty())
+        defaultValue
+    else {
+        val locale = Locale.getDefault()
+        val formatter = SimpleDateFormat(DATE_FORMATTER, locale)
+        val parse = SimpleDateFormat(DATE_FORMAT, locale).parse(this)
+        if (parse != null)
+            formatter.format(parse)
+        else defaultValue
+    }
 }
